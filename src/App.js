165ec36e29c299e652f45e52215d9b1c46835789
Bobby Johnson <rjohnson19@gmail.com>
2018-07-10 13:08:27 -0400

    Fetching real data from API.

--- src/App.js
+++ src/App.js
@@ -3,22 +3,10 @@ import './App.css';
 import Search from './Search';
 import Table from './Table';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-}, {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-}, ];
+const DEFAULT_QUERY = 'redux';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
 const username = 'rjohnson19';
 
@@ -28,13 +16,27 @@ class App extends Component {
     super(props);
 
     this.state = {
-      list,
       username,
-      searchTerm: ''
+      result: null,
+      searchTerm: DEFAULT_QUERY
     }
 
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
   }
 
   onSearchChange(event) {
@@ -43,15 +45,17 @@ class App extends Component {
 
   onDismiss(id) {
     const isNotId = (item) => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
+    const updatedHits = this.state.result.hits.filter(isNotId);
 
     this.setState({
-      list: updatedList
+      result: { ...this.state.result, hits: updatedHits }
     });
   }
 
   render() {
-    const {searchTerm, list, username} = this.state;
+    const {searchTerm, result, username} = this.state;
+    if (!result) { return null; }
+
     return (
       
       <div className="page">
@@ -65,7 +69,7 @@ class App extends Component {
         </Search>
         </div>
         <Table 
-          list={list}
+          list={result.hits}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
         />
