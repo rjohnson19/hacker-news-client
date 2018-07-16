409c01726c77d7af44bfa7e78f8a1ab4b6f203cf
Bobby Johnson <rjohnson19@gmail.com>
2018-07-16 09:33:28 -0400

    Added a few enzyme tests.

--- src/Button.test.js
+++ src/Button.test.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 import Button from "./Button";
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe("Button", () => {
   it("renders without crashing", () => {
@@ -14,5 +18,10 @@ describe("Button", () => {
     const component = renderer.create(<Button>Give Me More</Button>);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('renders the label text', () => {
+    const button = shallow(<Button>Give Me More</Button>);
+    expect(button.text()).toEqual('Give Me More');
   });
 });
--- src/Table.test.js
+++ src/Table.test.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 import Table from "./Table";
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe("Table", () => {
   const props = {
@@ -13,13 +17,18 @@ describe("Table", () => {
 
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<Table { ...props } />, div);
+    ReactDOM.render(<Table {...props} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   test("has a valid snapshot", () => {
-    const component = renderer.create(<Table { ...props } />);
+    const component = renderer.create(<Table {...props} />);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("shows two items in list", () => {
+    const table = shallow(<Table {...props} />);
+    expect(table.find(".table-row").length).toBe(2);
   });
 });
